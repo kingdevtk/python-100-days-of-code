b0aef90eb7701680c8a303241a91f875ce5856fe
kingdevtk <trina.t.king@gmail.com>
2023-04-10 17:31:40 -0400

    track python exercises

---
+++ automate_boring_stuff_python/flow_control.py
@@ -0,0 +1,56 @@
+# if, else, elif statements
+
+# name = 'Trina'
+# if name == 'Trina':
+#     print('Heeey Trina')
+# print('Done')
+
+# Say hi if your name is Trina
+
+# print('what is your name?') # ask for their name
+
+# name = input() # user enters their name
+
+# if name == 'Trina':
+#     print('Hi, ' + name)
+# elif name == 'Sally':
+#     print('You are not Trina.')
+# elif name == '':
+#     print('hellooooo, stranger')
+# else:
+#     print('you may be learning something after all.')
+
+
+# password = 'rose'
+# if password == 'swordfish':
+#     print('Access granted')
+# else:
+#     print('wrong password')
+
+
+
+# While Loops
+
+# spam = 0
+# while spam < 5: # this while loop iterates 5 times
+#     print('Hello world!')
+#     spam = spam + 1
+
+
+# name = ''
+# while name != 'your name':
+#     print('Please type your name.')
+#     name = input()
+# print('Thanks!')
+
+
+# break
+name = ''
+while True:
+    print('Please type your name.')
+    name = input()
+    if name == 'your name':
+        break
+print('Thanks!')
+
+

ddbe9a8b59eb0c297310275ddee1798125e43205
kingdevtk <trina.t.king@gmail.com>
2023-04-12 16:35:01 -0400

    day 2 of 100

---
+++ automate_boring_stuff_python/functions.py
@@ -0,0 +1,46 @@
+# built-in functions
+
+# import sys
+# print('Hello')
+# sys.exit()
+# print('Goodbye')
+
+
+# third party modules are available, look to appendix A in the book
+
+
+# writing my own functions
+
+# def hello(name):
+#     print('Yo! ' + name)
+
+# hello('Trina')
+# hello('Tishia')
+
+
+# def plusOne(number):
+#     return number + 1
+
+# newNumber = plusOne(5)
+# print(newNumber)
+
+
+# keyword values
+
+# print('Hiiii', 'World', sep='227')
+
+# print('Hello', end=' ')
+# print('World')
+
+# Scopes
+
+spam = 42  # global variable
+
+def eggs():
+    spam = 42 # local variable
+
+print('some code here')
+print('more code here')
+
+
+

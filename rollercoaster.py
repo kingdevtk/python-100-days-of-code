a137524b8df042b7fee0478c17a1d7716abc91ec
kingdevtk <trina.t.king@gmail.com>
2023-05-18 15:35:07 -0400

    completed day 3 work

--- rollercoaster.py
+++ rollercoaster.py
@@ -11,6 +11,9 @@ if height >= 120:
   elif age <= 18:
     bill = 7
     print('Youth tickets are $7.')
+  elif age == 45 or age <= 55:
+    bill = 0
+    print('You get to ride for free.')
   else:
     bill = 12
     print('Adult tickets are $12.')
